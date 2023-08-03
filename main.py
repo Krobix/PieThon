8f19fb58dc720cff3204748c5ce8c9c98080e10e
Krobix <brendon.shaw8921@gmail.com>
2023-08-03 18:46:56 -0400

    Finish

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pygame
+import pygame.freetype
 
 pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+font = pygame.freetype.Font(None, 64)
 running = True
 
 number = 1
 
 def draw_background():
-    # DRAW BACKGROUND CODE HERE
+    screen.fill("black")
+    #Putting circle here too I guess?
+    pygame.draw.circle(screen, "white", (640, 360), 310, width=3)
+    pygame.draw.line(screen, "white", (640, 50), (640, 670), width=3)
+    pygame.draw.line(screen, "white", (330, 360), (950, 360), width=3)
     return
 
 def draw_number():
     global number
-    # DRAW NUMBER CODE HERE
+    coords = [(640+155, 360-155), (640+155, 360+155), (640-155, 360+155), (640-155, 360-155)]
+    font.render_to(screen, coords[number-1], str(number), "white")
     return
 
 def update_number():
     global number
-    # UPDATE NUMBER CODE HERE
+    if number < 4:
+        number += 1
+    else:
+        number = 1
     return
 
 while running:
